abb32141becf5d64d42df990d5752fb6f2c0d859
Vincent <thanhvinhnguyenduc@gmail.com>
2023-02-17 14:17:55 +0700

    fixed bugs document handler

--- pysearch/documentHandler.py
+++ pysearch/documentHandler.py
@@ -3,32 +3,41 @@ import tokenization
 import os
 import json
 
-        
 
-def _set_name(doc_id):
-    """
-    Constructs a path where the document should be stored from doc_id
-    """
-    return os.path.join(PathSetUp.docs_path, tokenization.hash_name(doc_id), f"{doc_id}.json")
+class DocumentHandler(PathSetUp):
+    def __init__(self, base_directory) -> None:
+        super().__init__(base_directory)
 
-def save_document(doc_id, document):
-    """
-    Save the document
-    """
-    doc_path = _set_name(doc_id)
-    base_path = os.path.dirname(doc_path)
+    def _set_name(self, doc_id):
+        """
+        Constructs a path where the document should be stored from doc_id
+        """
+        return os.path.join(self.docs_path, tokenization.hash_name(doc_id), f"{doc_id}.json")
 
-    if not os.path.exists(base_path):
-        os.makedirs(base_path)
+    def save_document(self, doc_id, document):
+        """
+        Save the document
+        """
+        doc_path = self._set_name(doc_id)
+        base_path = os.path.dirname(doc_path)
 
-    with open(doc_path, 'w') as doc_file:
-        doc_file.write(json.dumps(document, ensure_ascii=False))
+        if not os.path.exists(base_path):
+            os.makedirs(base_path)
 
-def load_document(doc_id):
-    doc_path = _set_name(doc_id)
+        with open(doc_path, 'w') as doc_file:
+            doc_file.write(json.dumps(document, ensure_ascii=False))
 
-    with open(doc_path, 'r') as doc_file:
-        data = json.loads(doc_file.read())
+    def load_document(self, doc_id):
+        doc_path = self._set_name(doc_id)
 
-    return data
+        with open(doc_path, 'r') as doc_file:
+            data = json.loads(doc_file.read())
+
+        return data
+
+
+if __name__ == '__main__':
+    dh = DocumentHandler(os.path.join(os.getcwd(), "SearchData"))
+    dh.save_document('100', {1:2})
+    print(dh.load_document('100'))
 
